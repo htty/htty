e57fc048f0db02bd72e8505eea941ee7033c5027
Nils Jonsson <nils@alumni.rice.edu>
2010-11-21 20:17:17 -0600

    Use Enumerable#each_slice instead of writing our own

--- lib/htty/cli/commands/query_set.rb
+++ lib/htty/cli/commands/query_set.rb
@@ -63,24 +63,12 @@ class HTTY::CLI::Commands::QuerySet < HTTY::CLI::Command
     add_request_if_has_response do |request|
       self.class.notify_if_cookies_cleared request do
         escaped_arguments = escape_or_warn_of_escape_sequences(arguments)
-        in_groups_of(2, escaped_arguments).each do |key_value|
-          request.query_set(*key_value)
+        escaped_arguments.each_slice 2 do |name, value|
+          request.query_set name, value
         end
         request
       end
     end
-  end
-
-private
-
-  def in_groups_of(how_many, source)
-    groups = []
-    source = source.dup
-    (source.length / how_many).times do
-      groups << source.slice!(0, how_many)
-    end
-    groups << source unless source.empty?
-    groups
   end
 
 end
