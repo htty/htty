e1c947242f42368a5adbd8bc9731474889df90dd
Nils Jonsson <nils@alumni.rice.edu>
2012-01-26 02:05:45 -0600

    Fix deprecated Aruba and Cucumber constructs

--- features/step_definitions.rb
+++ features/step_definitions.rb
@@ -34,19 +34,19 @@ After do
 end
 
 Given 'an htty session' do
-  Given 'I run "htty" interactively'
+  step 'I run `htty` interactively'
 end
 
 Given /^an htty session with (.+)$/ do |address|
   hack_htty_to_use_vcr_cassette address
-  Given %Q{I run "htty #{address}" interactively}
+  step %Q{I run `htty #{address}` interactively}
 end
 
 Then 'I should see the 200 OK output' do
-  Then 'the output should match:',
+  step 'the output should match:',
        '200.+? OK -- \d+ headers? -- \d+-character body'
 end
 
 Then 'I should see the goodbye output' do
-  Then 'the output should contain:', '*** Happy Trails To You!'
+  step 'the output should contain:', '*** Happy Trails To You!'
 end
